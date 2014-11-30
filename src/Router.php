3025a96d79229ac21467ea695677cab141770b90
Vitaly Egorov <egorov@samsonos.com>
2014-11-30 15:05:36 +0200

    Created router service

---
+++ src/Router.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorov
+ * Date: 30.11.2014
+ * Time: 14:57
+ */
+
+namespace samson\router;
+
+/**
+ * Routing service
+ * @package samson\router
+ */
+class Router extends \samson\core\CompressableService
+{
+    
+} 
